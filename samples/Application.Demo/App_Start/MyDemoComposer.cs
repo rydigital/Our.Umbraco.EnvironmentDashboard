5f87611c4aef2dae561537e74741723209153879
Anthony <a.dang@ry.com>
2020-02-28 16:57:49 +0000

    Added demo composer

---
+++ samples/Application.Demo/App_Start/MyDemoComposer.cs
@@ -0,0 +1,37 @@
+﻿using Our.Umbraco.EnvironmentDashboard.Components;
+using Our.Umbraco.EnvironmentDashboard.Models;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Application.Demo.Start_Up
+{
+	public class MyDemoComposer : IUserComposer
+	{
+		public void Compose(Composition composition)
+		{
+			composition.Components().Append<MyDemoComponent>();
+		}
+	}
+
+	public class MyDemoComponent : IComponent
+	{
+		public void Initialize()
+		{
+			// Register as many environments as you wish
+			EnvironmentInfo.Instance.Domains.Add(new DomainInfo("Local", "environmentdashboard.localhost"));
+			EnvironmentInfo.Instance.Domains.Add(new DomainInfo("Development", "environmentdashboard-dev.localhost"));
+			EnvironmentInfo.Instance.Domains.Add(new DomainInfo("QA", "environmentdashboard-qa.localhost"));
+			EnvironmentInfo.Instance.Domains.Add(new DomainInfo("UAT", "environmentdashboard-qa.localhost"));
+			EnvironmentInfo.Instance.Domains.Add(new DomainInfo("Production", "environmentdashboard-prod.localhost"));
+
+			// Register my custom groups
+			var group = new InfoGroup("My Custom Group Title");
+			group.Pairs.Add(new InfoPair("my key", "my value"));
+			EnvironmentInfo.Instance.Groups.Add(group);
+		}
+
+		public void Terminate()
+		{
+		}
+	}
+}
